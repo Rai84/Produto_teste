79523bd8f5fd00c9c7b127016fa1b757c1606626
rodolfo.gsilva2 <rodolfo.gsilva2@senacsp.edu.br>
2022-09-02 20:03:39 -0300

    Adicionar arquivos de projeto.

---
+++ Produto_teste/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace Produto_teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Produto p = new Produto();
+           
+            Console.WriteLine("Entre com os dados do produto");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine(); 
+            
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), 
+                CultureInfo.InvariantCulture); 
+            
+             Console.Write("Quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine(), 
+                CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine("Digite o número de produtos a serem adicionados no estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+
+            Console.WriteLine("Dados atualizados: " + p);
+
+
+            Console.WriteLine("Digite o número de produtos a serem removidos do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+
+            Console.WriteLine("Dados atualizados: " + p);
+
+        }
+    }
+}
